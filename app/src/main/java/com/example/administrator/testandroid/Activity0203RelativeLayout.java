c944d107961741c7f2bb354f9e004b1146572f8f
locohuang <hpquan@126.com>
2017-03-02 08:57:03 +0800

    0203

---
+++ app/src/main/java/com/example/administrator/testandroid/Activity0203RelativeLayout.java
@@ -0,0 +1,13 @@
+package com.example.administrator.testandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Activity0203RelativeLayout extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activity0203_relative_layout);
+    }
+}
